a96e449eeba2f5f85bd741cf33b66c7f5f857a7c
blavenie <benoit.lavenier@e-is.pro>
2019-01-03 13:27:15 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 ROOT_DIR=$PWD
 TEMP_DIR=$PWD/tmp
-
-NW_VERSION=0.22.3
-#NW_VERSION=0.26.6
+NW_VERSION=0.35.3
+CHROMIUM_MAJOR_VERSION=71
+GCHANGE_DEFAULT_VERSION=0.8.3
 
 # Check first arguments = version
 if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+((a|b)[0-9]+)?$ ]];
 then
   VERSION="$1"
-  echo "Using given version: $VERSION"
+  echo "Using Gchange version: $VERSION"
 else
   if [[ -f $ROOT_DIR/src/nw/cesium/manifest.json ]];
   then
@@ -21,26 +21,60 @@ else
   then
     echo "Using detected version: $VERSION"
   else
-    VERSION="0.6.1";
-    echo "No version detected. Using default: $VERSION"
+    VERSION="${GCHANGE_DEFAULT_VERSION}";
+    echo "No Gchange version detected. Using default: $VERSION"
   fi
 fi
 
 mkdir -p $TEMP_DIR && cd $TEMP_DIR
 
+
+# Force nodejs version to 6
+if [[ -d "$NVM_DIR" ]]; then
+    . $NVM_DIR/nvm.sh
+    nvm use 6
+else
+    echo "nvm (Node version manager) not found (directory NVM_DIR not defined). Please install nvm, and retry"
+    exit -1
+fi
+
+mkdir -p $TEMP_DIR && cd $TEMP_DIR
+
+
 # Install NW.js
 if [[ ! -f $ROOT_DIR/src/nw/nw ]];
 then
-  wget http://dl.nwjs.io/v$NW_VERSION/nwjs-sdk-v$NW_VERSION-linux-x64.tar.gz
-  tar xvzf nwjs-sdk-v$NW_VERSION-linux-x64.tar.gz
-  mv nwjs-sdk-v$NW_VERSION-linux-x64/* "$ROOT_DIR/src/nw"
-  rm nwjs-sdk-v$NW_VERSION-linux-x64.tar.gz
-  rmdir nwjs-sdk-v$NW_VERSION-linux-x64
+  cd ${TEMP_DIR}
+
+  NV_BASENAME=nwjs-sdk
+  wget http://dl.nwjs.io/v$NW_VERSION/${NV_BASENAME}-v$NW_VERSION-linux-x64.tar.gz
+  tar xvzf ${NV_BASENAME}-v$NW_VERSION-linux-x64.tar.gz
+  mv ${NV_BASENAME}-v$NW_VERSION-linux-x64/* "$ROOT_DIR/src/nw"
+  rm ${NV_BASENAME}-v$NW_VERSION-linux-x64.tar.gz
+  rmdir ${NV_BASENAME}-v$NW_VERSION-linux-x64
   rmdir nw
+
+# Check NW version
+else
+  cd ${ROOT_DIR}/src/nw
+
+  NW_ACTUAL_VERSION=`./nw --version | grep nwjs | awk '{print $2}'`
+  echo "Using Chromium version: ${NW_ACTUAL_VERSION}"
+  CHROMIUM_ACTUAL_MAJOR_VERSION=`echo ${NW_ACTUAL_VERSION} | awk '{split($0, array, ".")} END{print array[1]}'`
+  cd ${ROOT_DIR}
+  if [[ ${CHROMIUM_ACTUAL_MAJOR_VERSION} -ne ${CHROMIUM_MAJOR_VERSION} ]]; then
+    echo "Bad Chromium major version: ${CHROMIUM_ACTUAL_MAJOR_VERSION}. Expected version ${CHROMIUM_MAJOR_VERSION}"
+    exit -1
+  fi
+
 fi
 
+# Install deps
+cd ${ROOT_DIR}/src/nw
+yarn
+
 # Remove old Cesium version
-if [[ -f $ROOT_DIR/src/nw/cesium/index.html ]];
+if [[ -f $ROOT_DIR/src/nw/gchange/index.html ]];
 then
   OLD_VERSION=`grep -oP "version\": \"\d+.\d+.\d+((a|b)[0-9]+)?\"" $ROOT_DIR/src/nw/gchange/manifest.json | grep -oP "\d+.\d+.\d+((a|b)[0-9]+)?"`
   if [[ ! "$VERSION" = "$OLD_VERSION" ]];
@@ -49,6 +83,8 @@ then
     rm -rf $ROOT_DIR/src/nw/gchange/fonts
     rm -rf $ROOT_DIR/src/nw/gchange/img
     rm -rf $ROOT_DIR/src/nw/gchange/lib
+    rm -rf $ROOT_DIR/src/nw/gchange/locale
+    rm -rf $ROOT_DIR/src/nw/gchange/sounds
     rm -rf $ROOT_DIR/src/nw/gchange/*.html
   fi
 fi
@@ -57,12 +93,20 @@ fi
 if [[ ! -f $ROOT_DIR/src/nw/cesium/index.html ]]; then
     echo "Downloading Gchange ${VERSION}..."
 
-    mkdir gchange_unzip && cd gchange_unzip
+    cd $TEMP_DIR
+    mkdir -p ${TEMP_DIR}/gchange_unzip && cd ${TEMP_DIR}/gchange_unzip
     wget "https://github.com/duniter-gchange/gchange-client/releases/download/v${VERSION}/gchange-v${VERSION}-web.zip"
+    if [[ ! $? -eq 0 ]]; then
+        echo "Could not download Gchange web release !"
+        exit -1;
+    fi
     unzip "gchange-v${VERSION}-web.zip"
     rm "gchange-v${VERSION}-web.zip"
+
+    # Add node.js file into HTML files
     sed -i 's/<script src="config.js"><\/script>/<script src="config.js"><\/script><script src="node.js"><\/script>/' "index.html"
     sed -i 's/<script src="config.js"><\/script>/<script src="config.js"><\/script><script src="node.js"><\/script>/' "debug.html"
+
     mv * "$ROOT_DIR/src/nw/gchange/"
     cd ..
     rmdir gchange_unzip
--- src/nw/gchange/node.js
+++ src/nw/gchange/node.js
@@ -12,9 +12,11 @@ const fs = requireNodejs('fs');
 const path = requireNodejs('path');
 const yaml = requireNodejs('js-yaml');
 const bs58 = requireNodejs('bs58');
+const clc = requireNodejs('cli-color');
+const gui = requireNodejs('nw.gui');
 
 Base58 = {
-  encode: (bytes) => bs58.encode(bytes),
+  encode: (bytes) => bs58.encode(new Buffer(bytes)),
   decode: (data) => new Uint8Array(bs58.decode(data))
 };
 
@@ -22,24 +24,13 @@ Base58 = {
 
 const HOME = requireNodejs('os').homedir();
 const DUNITER_HOME = path.resolve(HOME, '.config/duniter/duniter_default');
-const duniterConf = requireNodejs(path.resolve(DUNITER_HOME, 'conf.json'));
-const keyringRaw = fs.readFileSync(path.resolve(DUNITER_HOME, 'keyring.yml'));
-const keyring = yaml.safeLoad(keyringRaw)
-
-console.log('home = ', HOME);
-console.log('conf = ', duniterConf);
-console.log('keyring = ', keyring);
-
-const local_host = duniterConf.ipv4;
-const local_port = duniterConf.port
-const local_sign_pk = Base58.decode(keyring.pub);
-const local_sign_sk = Base58.decode(keyring.sec);
-
+const DUNITER_CONF = path.resolve(DUNITER_HOME, 'conf.json');
 const DEFAULT_GCHANGE_SETTINGS = {
   "useRelative": false,
   "timeWarningExpire": 2592000,
   "useLocalStorage": true,
   "rememberMe": true,
+  "useRemoteStorage": true,
   "plugins": {
     "es": {
       "enable": true,
@@ -54,36 +45,95 @@ const DEFAULT_GCHANGE_SETTINGS = {
   }
 };
 
-let settingsStr = window.localStorage.getItem('GCHANGE_SETTINGS');
-let dataStr = window.localStorage.getItem('GCHANGE_DATA');
-let settings = (settingsStr && JSON.parse(settingsStr));
-let data = (dataStr && JSON.parse(dataStr));
 
-let keyPairOK = data && data.keypair && data.keypair.signPk && data.keypair.signSk && true;
-if (keyPairOK) {
-  console.log('Trousseau Gchange déjà configuré, comparaison avec celui du nœud local...')
-  data.keypair.signPk.length = local_sign_pk.length;
-  data.keypair.signSk.length = local_sign_sk.length;
-  keyPairOK = Base58.encode(Array.from(data.keypair.signPk)) === keyring.pub
-    && Base58.encode(Array.from(data.keypair.signSk)) === keyring.sec
-    && data.pubkey === keyring.pub;
-  if (!keyPairOK) {
-    console.log('Le trousseau Gchange est différent du trousseau du nœud, imposons celui du nœud.')
-    // N.B. : ce comportement devrait être **confirmé** par l'utilisateur via une popup par ex.
-  } else {
-    console.log('Trousseaux identiques : pas de modification du trousseau Gchange à réaliser.')
+function isSdkMode () {
+  return gui && (window.navigator.plugins.namedItem('Native Client') !== null);
+}
+
+/**** Process command line args ****/
+var commands = gui && gui.App && gui.App.argv;
+var debug = false;
+if (commands && commands.length) {
+  for (i in commands) {
+    if (commands[i] === "--debug") {
+      console.log("[NW] Enabling debug mode (--debug)");
+      debug = true;
+
+      // Open the DEV tool (need a SDK version of NW)
+      if (isSdkMode()) {
+        gui.Window.get().showDevTools();
+      }
+    }
   }
 }
 
-if (duniterConf.currency === expectedCurrency
-  && (!data
-  || !keyPairOK
-  || settings.node.host != local_host
-  || settings.node.port != local_port)) {
-  if (confirm('Un nœud pour la monnaie ' + expectedCurrency + ' a été détecté sur cet ordinateur, voulez-vous que Gcahnge s\'y connecte ? (plus sécurisé)')) {
+/**** Re-routing console log ****/
+var oldConsole = {
+  log: console.log,
+  debug: console.debug,
+  info: console.info,
+  warn: console.warn,
+  error: console.error,
+}
+if (debug) {
+  console.debug = function (message) {
+    process.stdout.write(clc.green("[DEBUG] ") + message + "\n");
+    oldConsole.debug.apply(this, arguments);
+  };
+  console.log = function(message) {
+    process.stdout.write(clc.blue("[CONSOLE] ") + message + "\n");
+    oldConsole.log.apply(this, arguments);
+  }
+}
+console.info = function(message) {
+  process.stdout.write(clc.blue("[INFO]  ") + message + "\n");
+  oldConsole.info.apply(this, arguments);
+};
+console.warn = function(message) {
+  process.stdout.write(clc.yellow("[WARN]  ") + message + "\n");
+  oldConsole.warn.apply(this, arguments);
+};
+console.error = function(message) {
+  if (typeof message == "object") {
+    process.stderr.write(clc.red("[ERROR] ") + JSON.stringify(message) + "\n");
+  }
+  else {
+    process.stderr.write(clc.red("[ERROR] ") + message + "\n");
+  }
+  oldConsole.error.apply(this, arguments);
+};
+
+/**** Starting ****/
+let settingsStr = window.localStorage.getItem('GCHANGE_SETTINGS');
+let settings = settingsStr && JSON.parse(settingsStr);
+
+console.info("[NW] Starting. User home is {" + HOME + "}");
+
+const duniterConf = fs.existsSync(DUNITER_CONF) && requireNodejs(path.resolve(DUNITER_HOME, 'conf.json'));
+const local_host = duniterConf && duniterConf.ipv4;
+const local_port = duniterConf && duniterConf.port;
+
+
+if (duniterConf &&
+  duniterConf.currency === expectedCurrency
+  && (!settings
+    || settings.node.host != local_host
+    || settings.node.port != local_port)) {
+  // Detect locale
+  const locale = (settings && settings.locale && settings.locale.id).split('-')[0] || 'en';
+  console.debug('[NW] Using locale: ' + locale);
+  const confirmationMessage = (locale === 'fr') ?
+    'Un nœud pour la monnaie ' + expectedCurrency + ' a été détecté sur cet ordinateur, voulez-vous que Gchange s\'y connecte ?' :
+    'A node for currency ' + expectedCurrency + ' has been detected on this computer. Do you want Gchange to connect it?';
+
+  if (settings && settings.askLocalNodeKeyring === false) {
+    // Nothing to do
+    console.debug('[NW] Skipping Duniter local node confirmation (already asked)');
+
+  }
+  else if (confirm(confirmationMessage)) {
     settings = settings || DEFAULT_GCHANGE_SETTINGS;
-    data = data || {};
-    console.debug('Configuring Gchange...');
+    console.debug('[NW] Configuring Gchange on Duniter local node...');
     settings.node = {
       "host": local_host,
       "port": local_port
@@ -96,13 +146,13 @@ if (duniterConf.currency === expectedCurrency
         "port": "443"
       }
     };
-    settings.rememberMe = true;
-    data.pubkey = keyring.pub;
-    data.keypair = {
-      signPk: local_sign_pk,
-      signSk: local_sign_sk
-    };
     window.localStorage.setItem('GCHANGE_SETTINGS', JSON.stringify(settings));
-    window.localStorage.setItem('GCHANGE_DATA', JSON.stringify(data));
+  }
+  // User does NOT confirm: Not ask it again
+  else {
+    console.debug('[NW] User not need to connect on Duniter local node. Configuring Gchange to remember this choice...');
+    settings = settings || DEFAULT_CESIUM_SETTINGS;
+    settings.askLocalNodeKeyring = false;
+    window.localStorage.setItem('GCHANGE_SETTINGS', JSON.stringify(settings));
   }
 }
